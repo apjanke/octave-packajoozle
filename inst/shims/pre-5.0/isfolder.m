e64412c9190718bb7fab51e2c6c7f91f57976394
Andrew Janke <floss@apjanke.net>
2019-03-21 05:52:40 -0400

    Back-compatibility shims for Octave pre-5.0

---
+++ inst/shims/pre-5.0/isfolder.m
@@ -0,0 +1,25 @@
+## Copyright (C) 2019 Andrew Janke
+##
+## This file is part of Octave.
+##
+## Octave is free software: you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## Octave is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with Octave; see the file COPYING.  If not, see
+## <https://www.gnu.org/licenses/>.
+
+## Back-compatibility shim for isfolder.
+## This is a version of isfolder that will work on older versions of Octave
+## that don't have isfolder.
+
+function out = isfolder (f)
+  	out = exist (f, "dir");
+endfunction
